b97f1e9e49da623ed0231288597a9d54633ff3e5
Clark Green <clark.r.green@gmail.com>
2017-01-19 17:00:12 -0700

    boiler plate

---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/byuoitav/authmiddleware"
+	"github.com/byuoitav/hateoas"
+	"github.com/jessemillar/health"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	port := ":8004"
+	router := echo.New()
+	router.Pre(middleware.RemoveTrailingSlash())
+	router.Use(middleware.CORS())
+
+	// Use the `secure` routing group to require authentication
+	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
+
+	router.GET("/", echo.WrapHandler(http.HandlerFunc(hateoas.RootResponse)))
+	router.GET("/health", echo.WrapHandler(http.HandlerFunc(health.Check)))
+
+	// secure.GET("/:address/power/on", handlers.PowerOn)
+
+	server := http.Server{
+		Addr:           port,
+		MaxHeaderBytes: 1024 * 10,
+	}
+
+	router.StartServer(&server)
+}
